74ef0bd11979f35e83193d057375bb5f81c87d8d
SoumitSaha <saha.soumit884@gmail.com>
2025-03-14 22:34:18 -0400

    Fuzzing completed for BeautifulSoap4, Regex, Email Validator and requests pip module

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 
-from example_function1 import sample_function
+from bs4 import BeautifulSoup
 import unittest
 
-class TestCheckNumber(unittest.TestCase):
+class TestBeautifulSoupParsing(unittest.TestCase):
     def test_1(self):
-        sample_function('u‡gr(X')
+        try:
+            soup = BeautifulSoup('t<html><bodT><p>Initial test content</p>R</body></hml>', 'html.parser')
+            # Check if parsing the HTML creates a valid BeautifulSoup object
+            if soup is None:
+                raise ValueError("Invalid HTML structure")
+        except Exception as e:
+            with open("validate_bs4.txt", "a") as f:
+                f.write(f"Error for input:\n\n't<html><bodT><p>Initial test content</p>R</body></hml>'\n\nError: {str(e)}\n")
 
 if __name__ == "__main__":
     unittest.main()
-        
+    
